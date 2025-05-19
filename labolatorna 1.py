d301fbbd3206dcd769c23facd43558e2d6fc9edc
fedunvital <teroseke.fedun@gmail.com>
2025-05-20 01:13:27 +0300

    labolatorna 1.py

--- labolatorna 1.py
+++ labolatorna 1.py
@@ -1,42 +1,36 @@
 from abc import ABC, abstractmethod
 
-
-class Animal(ABC):
-    def __init__(self, name, age):
-        self.__name = name  
-        self.__age = age 
+class Vehicle(ABC):
+    def __init__(self, brand, model, year):
+        self.__brand = brand
+        self.__model = model
+        self.__year = year
 
     @abstractmethod
-    def make_sound(self):
+    def start_engine(self):
         pass
 
     def get_info(self):
-        return f"Ім'я: {self.__name}, Вік: {self.__age} років"
+        return f"Бренд: {self.__brand}, Модель: {self.__model}, Рік випуску: {self.__year}"
 
-    def set_age(self, new_age):
-        
-        if new_age > 0:
-            self.__age = new_age
+    def set_year(self, new_year):
+        if new_year >= 1900:
+            self.__year = new_year
 
-class Sheep(Animal):
-    def make_sound(self):
-        return "Бе Ме Бе Ме!"
+class YamahaTT600(Vehicle):
+    def start_engine(self):
+        return "Yamaha TT600 запускає двигун: Брррррр-вжжжж!"
 
-class Pig(Animal):
-    def make_sound(self):
-        return "Хрю-хрю!"
+class MitsubishiOutlander2009(Vehicle):
+    def start_engine(self):
+        return "Mitsubishi Outlander 2009 запускає двигун: Врум-врум!"
 
-class Cow(Animal):
-    def make_sound(self):
-        return "Мууу!"
-
-animals = [
-    Sheep("Бараш", 4),
-    Pig("Мейсо", 4),
-    Cow("Мілка", 5)
+vehicles = [
+    YamahaTT600("Yamaha", "TT600", 2003),
+    MitsubishiOutlander2009("Mitsubishi", "Outlander", 2009)
 ]
 
-for animal in animals:
-    print(animal.get_info())
-    print("Звук:", animal.make_sound())
+for vehicle in vehicles:
+    print(vehicle.get_info())
+    print("Звук двигуна:", vehicle.start_engine())
     print("-")
